d26cd9dfe138afc7cb3c68affd9900187e6f3208
Kayiira Edward <96061154+Kayiira-Edward@users.noreply.github.com>
2025-10-15 18:25:10 +0300

    Updated junior-dev-signature.php

--- junior-dev-signature.php
+++ junior-dev-signature.php
@@ -14,13 +14,18 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 // Task two : adding a custom footer message
-function jds_add_footer_message() {
-    if ( !is_admin() ) {
+function jds_add_footer_message() {//cb function
+    if ( !is_admin() ) {//only to show on the front end
     echo '<p>Built by a Junior Developer &copy; 2025</p>';
     }
 }
 add_action( 'wp_footer', 'jds_add_footer_message' );
 
-//Task 3 : modifying post tittles
-
-
+//Task 3 : modifying post title
+function jds_modify_title($title){//cb function
+    if(is_admin()){
+        return $title;//if admin return the original title
+    }//else add the signature
+    return $title . '  | Junior Dev Project';
+}
+add_filter('the_title', 'jds_modify_title');
